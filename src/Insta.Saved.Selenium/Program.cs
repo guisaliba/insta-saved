c1417bb1fcef9c050950a21dd6bdf11f59a5b9ea
Guilherme Saliba <salibagui19@gmail.com>
2025-01-10 22:27:39 -0300

    feat: add program entry point

---
+++ src/Insta.Saved.Selenium/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using OpenQA.Selenium.Chrome;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+      services.AddSingleton<IWebDriverFactory, ChromeDriverFactory>();
+      services.AddScoped<IInstagramService, InstagramService>();
+      services.AddScoped<ILoginHandler, LoginHandler>();
+    })
+    .Build();
+
+var instagramService = host.Services.GetRequiredService<IInstagramService>();
+
+await instagramService.RunAsync();
